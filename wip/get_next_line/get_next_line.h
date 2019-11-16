0d35f049c387068d04fd5e666a18c2ff0e85f12e
Caceresenzo <caceresenzo1502@gmail.com>
2019-11-16 22:31:04 +0100

    WIP: get_next_line: Added multi-fd support

--- wip/get_next_line/get_next_line.h
+++ wip/get_next_line/get_next_line.h
@@ -14,12 +14,28 @@
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
+# include <stdio.h>
 # include <stdlib.h>
 # include <fcntl.h>
+# include <string.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# ifndef NULL
+#  define NULL ((void *)0)
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX (255)
+# endif
 
 # define GNL_READ 1
 # define GNL_END_REACHED 0
 # define GNL_ERROR -1
+
+# define GNL_BSIZE0_PROC 1
 
 # define CHECK_PTR_DEF(ptr, def) if (!(ptr)) return (def);
 # define CHECK_PTR(ptr) CHECK_PTR_DEF(ptr, NULL);
@@ -28,6 +44,17 @@
 
 # define MIN(a, b) (((a) < (b)) ? (a) : (b))
 # define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+typedef struct				s_gnl_holder
+{
+	int				initialized;
+	int				reached;
+	unsigned char	buffer[BUFFER_SIZE];
+	size_t			byte_read;
+	size_t			offset;
+	unsigned char	*stored;
+	size_t			stored_length;
+}							t_gnl_holder;
 
 int		get_next_line(int fd, char **line);
 
