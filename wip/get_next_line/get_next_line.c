b2c7ab9a9ea0390854842473976cdf7298a45531
Caceresenzo <caceresenzo1502@gmail.com>
2019-11-16 22:29:52 +0100

    WIP: get_next_line: Added project

---
+++ wip/get_next_line/get_next_line.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecaceres <ecaceres@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/06 15:27:24 by ecaceres          #+#    #+#             */
+/*   Updated: 2019/11/06 15:27:24 by ecaceres         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 64
+#endif
+
+unsigned char	*internal_gnl_store(unsigned char c, int force)
+{
+	static unsigned char	*stored = NULL;
+	static size_t			stored_length = 0;
+	unsigned char			*new;
+
+	CHECK_MALLOC(new, (stored_length + 1 + force) * sizeof(char));
+	if (stored != NULL)
+	{
+		ft_memcpy(new, stored, stored_length);
+		free(stored);
+	}
+	if (c == '\n' || force)
+	{
+		new[stored_length] = '\0';
+		new[stored_length + force] = '\0';
+		stored = NULL;
+		stored_length = 0;
+		return (new);
+	}
+	new[stored_length] = c;
+	stored_length++;
+	stored = new;
+	return (NULL);
+}
+
+int				internal_gnl_handle(unsigned char buffer[BUFFER_SIZE],
+									size_t byte_read, char **return_line,
+									size_t *offset)
+{
+	unsigned char	*line;
+	int				force;
+
+	while (*offset < BUFFER_SIZE)
+	{
+		force = *offset >= byte_read;
+		line = internal_gnl_store(buffer[*offset], force);
+		*offset += 1;
+		if (line != NULL)
+		{
+			if (force)
+				*offset = BUFFER_SIZE;
+			*return_line = (char *)line;
+			return (1 + force);
+		}
+	}
+	return (0);
+}
+
+int				get_next_line(int fd, char **line)
+{
+	static unsigned char	buffer[BUFFER_SIZE];
+	static size_t			byte_read = 0;
+	static size_t			offset = BUFFER_SIZE;
+	int						code;
+
+	if (line == NULL)
+		return (GNL_END_REACHED);
+	*line = NULL;
+	while (1)
+	{
+		if (offset == BUFFER_SIZE)
+		{
+			byte_read = read(fd, buffer, BUFFER_SIZE);
+			offset = 0;
+		}
+		if (byte_read == (size_t)-1)
+			return (GNL_ERROR);
+		if ((code = internal_gnl_handle(buffer, byte_read, line, &offset)))
+		{
+			if (byte_read == 0 || code == 2)
+				return (GNL_END_REACHED);
+			return (GNL_READ);
+		}
+	}
+}
---
+++ wip/get_next_line/get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecaceres <ecaceres@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/06 15:27:31 by ecaceres          #+#    #+#             */
+/*   Updated: 2019/11/06 15:27:31 by ecaceres         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# define GNL_READ 1
+# define GNL_END_REACHED 0
+# define GNL_ERROR -1
+
+# define CHECK_PTR_DEF(ptr, def) if (!(ptr)) return (def);
+# define CHECK_PTR(ptr) CHECK_PTR_DEF(ptr, NULL);
+# define CHECK_PTR_EMPTY(ptr) if (!(ptr)) return ;
+# define CHECK_MALLOC(var, size) CHECK_PTR((var = malloc(size)))
+
+# define MIN(a, b) (((a) < (b)) ? (a) : (b))
+# define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+int		get_next_line(int fd, char **line);
+
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+
+#endif
