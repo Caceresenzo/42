af3bc3c068ba13dd3de808e2c644e497bb1a3d61
Caceresenzo <caceresenzo1502@gmail.com>
2019-12-13 17:10:59 +0100

    WIP: libasm: Recoded ft_strdup

--- wip/libasm/ft_strdup.s
+++ wip/libasm/ft_strdup.s
@@ -6,44 +6,40 @@ extern _ft_strcpy
 
 section .text
 
+; Memory Map
+; +----------+------------+--------------+----------+------+----+--------+
+; |  origin  |   local    |     type     |   size   | from | to | offset |
+; +----------+------------+--------------+----------+------+----+--------+
+; | argument | s1         | const char * | 8 (ptr)  |    0 |  8 |     24 |
+; | variable | length     | size_t       | 8 (long) |    8 | 16 |     16 |
+; | variable | duplicated | char *       | 8 (ptr)  |   16 | 24 |      8 |
+; +----------+------------+--------------+----------+------+----+--------+
+
 _ft_strdup:
-	push r12
-	push r13
-	push r14
+	push	rbp							; Saving register: rbp
 	
-	mov r12, rdi					; // Argument 1: (const char *) s1
+	mov		rbp, rsp					; Saving stack pointer: rsp
+	sub		rsp, 32						; Reserving space (% 16)
+	
+	mov		QWORD [rbp - 24], rdi		; Storing in [ 0 to 8 ] <- arg1: (const char *) s1
 
-	mov rdi, r12					; passing parameter: s
-	call _ft_strlen					; calling ft_strlen
-	mov r13, rax					; storing result as "length"
-	inc r13							; length++
-	imul r13, r13, 1				; length *= sizeof(char)
+	mov		rax, QWORD [rbp - 24]		; rax = [ 0 to 8 ] -> s1
+	mov		rdi, rax					; Setting argument 's' = rax
+	call	_ft_strlen					; Calling ft_strlen
+	mov		QWORD [rbp - 16], rax		; Storing in [ 8 to 16 ] <- return: (size_t)
 
-	mov rdi, r13					; passing parameter: size = length
-	call _malloc					; calling malloc
-	mov r14, rax					; storing result as "duplicated"
-	
-	cmp r14, 0						; cmp: duplicated == 0
-	je malloc_fail					; if true: goto malloc_fail
-	jne copy						; if false: goto copy
-	
-	malloc_fail:
-		mov rax, 0					; set return value = NULL
-		jmp end
-		
-	copy:
-		mov rdi, r14				; passing parameter: dst = duplicated
-		mov rsi, r12				; passing parameter: src = s1
-		call _ft_strcpy				; calling ft_strcpy
-		mov r14, rax				; getting returned value (just is case)
-		mov rax, r14				; set returned value = ft_strcpy's return value
-		jmp end
-	
-	end:
-		;
+	mov		rax, QWORD [rbp - 16]		; rax = [ 8 to 16 ] -> length
+	mov		rdi, rax					; Setting argument 'size' = length
+	call	_malloc						; Calling malloc
+	mov		QWORD [rbp - 8], rax		; Storing in [ 12 to 20 ] <- return: (size_t)
 
-	pop r14
-	pop r13
-	pop r12
-	
-	ret								; do return
+	mov		rax, QWORD [rbp - 8]		; rax = [ 16 to 24 ] -> duplicated
+	mov		rdi, rax					; Setting argument 'dest' = rax
+	mov		rax, QWORD [rbp - 24]		; rax = [ 0 to 8 ] -> s1
+	mov		rsi, rax					; Setting argument 'src' = rax
+	call	_ft_strcpy					; Calling ft_strcpy
+
+	mov		rax, QWORD [rbp - 8]		; Setting returned value to [ 16 to 24 ]
+
+	leave								; Releasing space
+	ret									; Returning
