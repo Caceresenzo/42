1d18ea821da15e72d481a800eb3ced456e0e6944
Caceresenzo <caceresenzo1502@gmail.com>
2019-12-13 13:36:06 +0100

    WIP: ft_server: Added SSL support

--- wip/ft_server/srcs/wp-config.php
+++ wip/ft_server/srcs/wp-config.php
@@ -19,11 +19,11 @@
  */
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress_42' );
+define( 'DB_NAME', '%MYSQL_DATABASE%' );
 /** MySQL database username */
 define( 'DB_USER', 'root' );
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password' );
+define( 'DB_PASSWORD', '%MYSQL_PASSWORD%' );
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
 /** Database Charset to use in creating database tables. */
