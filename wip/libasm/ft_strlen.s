ac7c657e72515143e2cdd9ccf3f047587dd37e48
Caceresenzo <caceresenzo1502@gmail.com>
2019-12-14 17:23:11 +0100

    WIP: libasm: Recoded ft_strlen

--- wip/libasm/ft_strlen.s
+++ wip/libasm/ft_strlen.s
@@ -1,25 +1,39 @@
 global _ft_strlen
 section .text
 
-_ft_strlen:
-	push r12
+; Memory Map
+; +----------+--------+--------+----------+------+----+--------+
+; |  origin  | local  |  type  |   size   | from | to | offset |
+; +----------+--------+--------+----------+------+----+--------+
+; | argument | s      | char * | 8 (ptr)  |    0 |  8 |     24 |
+; | variable | length | size_t | 8 (long) |    8 | 16 |     16 |
+; +----------+--------+--------+----------+------+----+--------+
 
-	mov r12, rdi					; // Argument 1: (char *) s
-	mov rax, 0						; set returned value = 0
+_ft_strlen:
+	push	rbp							; Saving register: rbp
+
+	mov		rbp, rsp					; Saving stack pointer: rsp
+	sub		rsp, 32						; Reserving space (% 16)
+	
+	mov		QWORD [rbp - 24], rdi		; Storing in [ 0 to 8 ] <- arg1: (const char *) s
+	mov		QWORD [rbp - 16], 0			; Storing in [ 8 to 16 ] <- 'length' = 0
 
 	loop:
-		cmp BYTE [r12], 0			; cmp: *s == 0
-		je endloop					; if true: goto endloop
-		jne increment				; if false: goto increment
+		mov		rax, QWORD [rbp - 24]	; rax = [ 0 to 8 ] -> s
+		movzx	eax, BYTE [rax]			; Casted content of rax as BYTE
+		cmp		eax, 0					; Condition
+		jne		increment				; if (*s != 0): goto increment
+		jmp		endloop					; goto endloop
 
 	increment:
-		inc rax						; returned value++
-		inc r12						; s++
-		jmp loop					; goto: loop
+		add		QWORD [rbp - 24], 1		; Increment memory stored in [ 0 to 8 ] -> s
+		add		QWORD [rbp - 16], 1		; Increment memory stored in [ 8 to 16 ] -> 'length'
+		jmp		loop					; goto: loop
 
 	endloop:
 		;
 	
-	pop r12
+	mov		rax, QWORD [rbp - 16]		; Setting returned value to [ 8 to 16 ] -> 'length'
 
-	ret								; do return
+	leave								; Releasing space
+	ret									; Returning
