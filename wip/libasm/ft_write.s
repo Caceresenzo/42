3d4b50599a2b05daa712095d082656ad7b49e7fd
Caceresenzo <caceresenzo1502@gmail.com>
2019-12-16 11:12:16 +0100

    WIP: libasm: Recoded ft_write

--- wip/libasm/ft_write.s
+++ wip/libasm/ft_write.s
@@ -1,29 +1,37 @@
 global _ft_write
 section .text
 
+; Memory Map
+; +----------+--------+--------+----------+------+----+--------+
+; |  origin  | local  |  type  |   size   | from | to | offset |
+; +----------+--------+--------+----------+------+----+--------+
+; | argument | fildes | int    | 4 (ptr)  |    0 |  4 |     28 |
+; | argument | buf    | void * | 8 (ptr)  |    4 | 12 |     20 |
+; | argument | nbyte  | size_t | 8 (long) |   12 | 20 |     12 |
+; +----------+--------+--------+----------+------+----+--------+
+
 ; https://filippo.io/linux-syscall-table/ 
 ; https://opensource.apple.com/source/xnu/xnu-1504.3.12/bsd/kern/syscalls.master
 
 ; https://stackoverflow.com/a/48845698/7292958 (reason of the 0x2000000 offset)
 
 _ft_write:
-	push r12
-	push r13
-	push r14
-	
-	mov r12, rdi					; // Argument 1: (int) fildes
-	mov r13, rsi					; // Argument 2: (void *) buf
-	mov r14, rdx					; // Argument 3: (size_t) nbyte
+	push	rbp							; Saving register: rbp
 
-	mov rax, 0x2000004				; set to syscall #4: write
+	mov		rbp, rsp					; Saving stack pointer: rsp
+	sub		rsp, 32						; Reserving space (% 16)
 
-	mov rdi, r12					; passing parameter: file descriptor
-	mov rsi, r13					; passing parameter: buffer
-	mov rdx, r14					; passing parameter: length
-	syscall							; do syscall
+	mov		DWORD [rbp - 28], edi		; Storing in [ 0 to 8 ] <- arg1: (int) fildes
+	mov		QWORD [rbp - 20], rsi		; Storing in [ 8 to 16 ] <- arg2: (void *) buf
+	mov		QWORD [rbp - 12], rdx		; Storing in [ 8 to 16 ] <- arg3: (size_t) nbyte
 
-	pop r14
-	pop r13
-	pop r12
+	mov		rax, 0x2000004				; Setting syscall ID to #4: 'write'
 
-	ret								; do return
+	mov edi, DWORD [rbp - 28]			; Passing parameter: 'file fildes'
+	mov rsi, QWORD [rbp - 20]			; Passing parameter: 'buf'
+	mov rdx, QWORD [rbp - 12]			; Passing parameter: 'nbyte'
+
+	syscall								; Doing syscall
+
+	leave								; Releasing space
+	ret									; Returning
