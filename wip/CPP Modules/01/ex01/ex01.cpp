1036da3c9fdc4a43b36eaab366163130ec7fc440
Caceresenzo <caceresenzo1502@gmail.com>
2020-01-03 12:19:14 +0100

    WIP: CPP Module 01: Added ex01

---
+++ wip/CPP Modules/01/ex01/ex01.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex01.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecaceres <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/03 11:32:52 by ecaceres          #+#    #+#             */
+/*   Updated: 2020/01/03 11:32:56 by ecaceres         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+void
+memoryLeak()
+{
+	std::string* panthere = new std::string("String panthere");
+	std::cout << *panthere << std::endl;
+
+	delete panthere;
+}
