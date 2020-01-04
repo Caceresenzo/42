d06d72963238d7b4b8fd796f79ae77ec1833c235
Caceresenzo <caceresenzo1502@gmail.com>
2020-01-04 11:39:38 +0100

    WIP: CPP Module 01: Added ex08

---
+++ wip/CPP Modules/01/ex08/Human.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Human.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecaceres <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/03 11:32:52 by ecaceres          #+#    #+#             */
+/*   Updated: 2020/01/03 11:32:56 by ecaceres         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HUMAN_HPP
+# define HUMAN_HPP
+
+# include <string>
+
+class Human
+{
+	private:
+		void meleeAttack(std::string const &target);
+		void rangedAttack(std::string const &target);
+		void intimidatingShout(std::string const &target);
+
+	public:
+		void action(std::string const &action_name, std::string const &target);
+};
+
+#endif /* HUMAN_HPP_ */
