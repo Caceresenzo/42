e927344144806eece5ae97bb4ea9581531b2504d
Caceresenzo <caceresenzo1502@gmail.com>
2020-01-25 21:23:35 +0100

    WIP: ft_services: Added grafana, influxdb and a generator

--- wip/ft_services/setup.sh
+++ wip/ft_services/setup.sh
@@ -1,19 +1,95 @@
-goinfre=~/goinfre
-minikube_home=$goinfre"/minikube"
+export GOINFRE=~/goinfre
+export MINIKUBE_HOME=$GOINFRE/minikube
 
-if [[ -d "$minikube_home" ]]
+export SOURCES=srcs
+
+export LOGS=$SOURCES/logs
+
+export CERTIFICATES=$SOURCES/certificates
+
+export KUSTOMIZATION=$SOURCES/kustomization
+export KUSTOMIZATION_READY=$KUSTOMIZATION/ready
+export KUSTOMIZATION_TEMPLATES=$KUSTOMIZATION/templates
+
+export OPERATION=$1
+
+function redirect_minikube()
+{
+	to=$1
+
+	if [[ -d "$to" ]]
+	then
+		echo "Using redirected MINIKUBE home"
+	else
+		echo "Redirecting MINIKUBE home directory... ($to)"
+		mkdir "$to"
+	fi
+}
+
+function start_minikube()
+{
+	#redirect_minikube
+	
+	minikube status | grep -q Stopped
+	if [[ $? -eq "0" ]]
+	then
+		minikube config set vm-driver virtualbox
+		minikube start --memory 3000
+	fi
+	
+	kill -9 $(ps aux | grep "[m]inikube dashboard" | cut -c10-15 | tr -d ' ') || true
+	minikube dashboard &
+}
+
+function generate_tls_certificate()
+{
+	cert_file=$1
+	key_file=$2
+	to=$3
+
+﻿	rm -rf "${cert_file}" "${key_file}"
+	
+	openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out "${cert_file}" -keyout "${key_file}" -subj "/C=FR/ST=France/L=France/OU=42/OU=42/CN=42"
+	
+	sed -i 's/__b64_cert__/'$(cat $cert_file | base64 | tr -d '\n')'/g' $to
+	sed -i 's/__b64_key__/'$(cat $key_file | base64 | tr -d '\n')'/g' $to
+}
+
+function generate_ready_kustomization()
+{
+	from=$1
+	to=$2
+	
+	clear_ready $to
+	cp $from/*.yaml $to/
+	
+	ip=$(minikube ip)
+	
+	find $to/ -name "*.yaml" -type f -print -exec sed -i 's/__ip__/'$ip'/g' {} \;
+}
+
+function clear_ready()
+{
+	directory=$1
+	
+	rm -rf $directory/*.yaml
+}
+
+if [[ "$OPERATION" != "update" ]]
 then
-	echo "using redirected minikube home"
-else
-	echo "redirecting minikube home directory..."
-	mkdir "$minikube_home"
+	start_minikube
+
+	cp $KUSTOMIZATION_TEMPLATES/tls.yaml $KUSTOMIZATION/tls.yaml
+	generate_tls_certificate $CERTIFICATES/common.crt $CERTIFICATES/common.key $KUSTOMIZATION/tls.yaml
 fi
 
-eval 'export MINIKUBE_HOME=$minikube_home'
+generate_ready_kustomization $KUSTOMIZATION $KUSTOMIZATION_READY
 
-minikube config set vm-driver virtualbox
-minikube start
+if [[ "$OPERATION" == "delete" ]]
+then
+	kubectl delete -k $KUSTOMIZATION_READY
+else
+	kubectl apply -k $KUSTOMIZATION_READY
+fi
 
-minikube dashboard &
-
-kubectl apply -k srcs
+clear_ready $KUSTOMIZATION_READY
