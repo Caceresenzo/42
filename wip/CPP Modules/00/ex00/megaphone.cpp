0ba1a06f2be4173c1ae5ade9e779ce4bfb794bdf
Caceresenzo <caceresenzo1502@gmail.com>
2020-06-15 11:02:28 +0200

    WIP: CPP Module 00: ex01: Updated code

--- wip/CPP Modules/00/ex00/megaphone.cpp
+++ wip/CPP Modules/00/ex00/megaphone.cpp
@@ -10,28 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ctype.h>
 #include <iostream>
 
 #define DEFAULT_MESSAGE "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
 int
-	main(int argc, char **argv) {
+main(int argc, char **argv)
+{
 	if (argc <= 1)
 		std::cout << DEFAULT_MESSAGE << std::endl;
 	else
 	{
-		for (int index = 1; index < argc; index++) {
+		for (int index = 1; index < argc; index++)
+		{
 			char *str = argv[index];
 			char current;
 
-			for (int jndex = 0; (current = str[jndex]); jndex++) {
-				if (current >= 'a' && current <= 'z')
-				{
-					current -= 0x20;
-				}
-
-				std::cout << current;
-			}
+			for (int jndex = 0; (current = str[jndex]); jndex++)
+				std::cout << (char) toupper(current);
 		}
 
 		std::cout << std::endl;
