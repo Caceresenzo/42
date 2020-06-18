ca32232cfe4695719b08b2b6d7d75cbc8d45ba3b
Caceresenzo <caceresenzo1502@gmail.com>
2020-06-18 11:49:36 +0200

    WIP: CPP Module 01: ex10: Fixed various bug

--- wip/CPP Modules/01/ex10/main.cpp
+++ wip/CPP Modules/01/ex10/main.cpp
@@ -13,7 +13,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <errno.h>
+#include <cerrno>
 
 #ifndef PROGRAM_NAME
 # define PROGRAM_NAME "cato9tails"
@@ -21,31 +21,13 @@
 
 int g_exitCode = 0;
 
-
-/*
-** Note:
-** It seems that thanks to unix, /dev/stdin is not a file, and so, is not supported by the ifstream...
-**
-** https://github.com/bigartm/bigartm/pull/258#issuecomment-128131871
-**
-** Its ugly, very ugly, way too ugly, but "it works"...
-** But does not handle other /dev/ files. (e.g. /dev/tty)
-*/
-
-bool
-isUnixStdFile(std::string file)
-{
-	return (file == "/dev/stdin" || file == "/dev/stdout" || file == "/dev/stderr"
-			|| file == "/dev/fd/0" || file == "/dev/fd/1" || file == "/dev/fd/2");
-}
-
 void
-displayErrno(std::string file)
+displayErrno(std::string &file)
 {
 	if (errno)
 	{
 		g_exitCode = 1;
-		std::cerr << PROGRAM_NAME << ": " << file << ": " << strerror(errno) << std::endl;
+		std::cerr << PROGRAM_NAME << ": " << file << ": " << std::strerror(errno) << std::endl;
 	}
 }
 
@@ -54,22 +36,18 @@ handleStream(std::string file, std::istream &stream)
 {
 	std::string read;
 
+	stream.peek();
+	if (!stream.good())
+		displayErrno(file);
 	while (stream.good())
 	{
 		std::getline(stream, read);
 		std::cout << read;
 
 		if (stream.eof())
-		{
 			break;
-		}
 
 		std::cout << std::endl;
-	}
-
-	if (!stream.good())
-	{
-		displayErrno(file);
 	}
 }
 
@@ -77,24 +55,22 @@ int
 main(int argc, char **argv)
 {
 	if (argc == 1)
-	{
 		handleStream("", std::cin);
-	}
 	else
 	{
+		std::ifstream stream;
+
 		for (int index = 1; index < argc; index++)
 		{
 			errno = 0;
 
 			std::string file = std::string(argv[index]);
 
-			if (isUnixStdFile(file))
-			{
+			if (file == "-")
 				handleStream(file, std::cin);
-			}
 			else
 			{
-				std::ifstream stream(file);
+				stream.open(file, std::ifstream::in);
 
 				if (stream.is_open())
 				{
@@ -102,9 +78,7 @@ main(int argc, char **argv)
 					stream.close();
 				}
 				else
-				{
 					displayErrno(file);
-				}
 			}
 		}
 	}
