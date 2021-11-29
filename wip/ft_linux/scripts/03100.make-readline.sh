b24c84fc1a2e5dd8526f318e92fe0baf72617029
Caceresenzo <caceresenzo1502@gmail.com>
2021-11-29 23:31:41 +0100

    WIP: ft_linux: Make `readline`

---
+++ wip/ft_linux/scripts/03100.make-readline.sh
@@ -0,0 +1,28 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+
+cd /sources
+
+tar xvf readline-*.tar*
+cd readline-*/
+
+sed -i '/MV.*old/d' Makefile.in
+sed -i '/{OLDSUFF}/c:' support/shlib-install
+
+./configure --prefix=/usr    \
+            --disable-static \
+            --with-curses    \
+            --docdir=/usr/share/doc/readline-8.1
+
+make SHLIB_LIBS="-lncursesw"
+
+make SHLIB_LIBS="-lncursesw" install
+
+install -v -m644 doc/*.{ps,pdf,html,dvi} /usr/share/doc/readline-8.1
+
+___END
