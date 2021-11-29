7dcdcb2d063583ca31fa386389bf07b01d50767b
Caceresenzo <caceresenzo1502@gmail.com>
2021-11-29 23:31:21 +0100

    WIP: ft_linux: Make `bzip2`

---
+++ wip/ft_linux/scripts/03060.make-bzip2.sh
@@ -0,0 +1,37 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+
+cd /sources
+
+tar xvf bzip2-*.tar*
+cd bzip2-*/
+
+patch -Np1 -i ../bzip2-*.patch
+
+sed -i 's@\(ln -s -f \)\$(PREFIX)/bin/@\1@' Makefile
+
+sed -i "s@(PREFIX)/man@(PREFIX)/share/man@g" Makefile
+
+make -f Makefile-libbz2_so
+make clean
+
+make
+
+make PREFIX=/usr install
+
+cp -av libbz2.so.* /usr/lib
+ln -sv libbz2.so.* /usr/lib/libbz2.so
+
+cp -v bzip2-shared /usr/bin/bzip2
+for i in /usr/bin/{bzcat,bunzip2}; do
+  ln -sfv bzip2 \$i
+done
+
+rm -fv /usr/lib/libbz2.a
+
+___END
