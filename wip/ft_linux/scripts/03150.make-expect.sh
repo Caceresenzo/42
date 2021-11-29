e33348155274e17b572b84d2d1f516b7491865b6
Caceresenzo <caceresenzo1502@gmail.com>
2021-11-29 23:34:58 +0100

    WIP: ft_linux: Make `expect`

---
+++ wip/ft_linux/scripts/03150.make-expect.sh
@@ -0,0 +1,29 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf expect*.tar*
+cd expect*/
+
+./configure --prefix=/usr           \
+            --with-tcl=/usr/lib     \
+            --enable-shared         \
+            --mandir=/usr/share/man \
+            --with-tclinclude=/usr/include
+
+make
+
+make test
+
+make install
+
+ln -svf expect*/libexpect*.so /usr/lib
+
+___END
