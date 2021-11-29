57a54af7452d69c6574e98f37846fae18ea6677b
Caceresenzo <caceresenzo1502@gmail.com>
2021-11-29 23:01:12 +0100

    WIP: ft_linux: Make `iana etc`

---
+++ wip/ft_linux/scripts/03020.make-iana-etc.sh
@@ -0,0 +1,16 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+
+cd /sources
+
+tar xvf iana-etc-*.tar*
+cd iana-etc-*/
+
+cp services protocols /etc
+
+___END
