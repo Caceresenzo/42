94366c1afbfc1d9781f352212a032ceca63283d4
Caceresenzo <caceresenzo1502@gmail.com>
2021-11-29 23:35:05 +0100

    WIP: ft_linux: Make `deja gnu`

---
+++ wip/ft_linux/scripts/03160.make-deja-gnu.sh
@@ -0,0 +1,28 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf dejagnu-*.tar*
+cd dejagnu-*/
+
+mkdir -vp build
+cd        build
+
+../configure --prefix=/usr
+makeinfo --html --no-split -o doc/dejagnu.html ../doc/dejagnu.texi
+makeinfo --plaintext       -o doc/dejagnu.txt  ../doc/dejagnu.texi
+
+make install
+install -v -dm755  /usr/share/doc/dejagnu-*
+install -v -m644   doc/dejagnu.{html,txt} /usr/share/doc/dejagnu-*
+
+make check
+
+___END
