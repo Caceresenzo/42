1777f8c07ed7dbe5d1fba3e716a47d9d9fb7dd0d
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-13 16:01:43 +0100

    WIP: ft_linux: Make `pkg config`

---
+++ wip/ft_linux/scripts/03260.make-pkg-config.sh
@@ -0,0 +1,26 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf pkg-config-*.tar*
+cd pkg-config-*/
+
+./configure --prefix=/usr              \
+            --with-internal-glib       \
+            --disable-host-tool        \
+            --docdir=/usr/share/doc/pkg-config-0.29.2
+
+make
+
+make check
+
+make install
+
+___END
