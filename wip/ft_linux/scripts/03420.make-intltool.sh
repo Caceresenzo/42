81b8e06b4012698095207f45c4afcf5afaeebdf9
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-13 16:07:26 +0100

    WIP: ft_linux: Make `intltool`

---
+++ wip/ft_linux/scripts/03420.make-intltool.sh
@@ -0,0 +1,26 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf intltool-*.tar*
+cd intltool-*/
+
+sed -i 's:\\\${:\\\$\\{:' intltool-update.in
+
+./configure --prefix=/usr
+
+make
+
+make check
+
+make install
+install -v -Dm644 doc/I18N-HOWTO /usr/share/doc/intltool/I18N-HOWTO
+
+___END
