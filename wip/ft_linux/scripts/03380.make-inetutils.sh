795a2f7e28971179db93af511c8a7c9e1825d197
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-13 16:03:52 +0100

    WIP: ft_linux: Make `inetutils`

---
+++ wip/ft_linux/scripts/03380.make-inetutils.sh
@@ -0,0 +1,34 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf inetutils-*.tar*
+cd inetutils-*/
+
+./configure --prefix=/usr        \
+            --bindir=/usr/bin    \
+            --localstatedir=/var \
+            --disable-logger     \
+            --disable-whois      \
+            --disable-rcp        \
+            --disable-rexec      \
+            --disable-rlogin     \
+            --disable-rsh        \
+            --disable-servers
+
+make
+
+make check
+
+make install
+
+mv -v /usr/{,s}bin/ifconfig
+
+___END
