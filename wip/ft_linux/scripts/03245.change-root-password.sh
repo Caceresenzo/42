eb8a6e5502ab55f15ef95a2dd619b209c060553e
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-13 15:59:39 +0100

    WIP: ft_linux: Change root password

---
+++ wip/ft_linux/scripts/03245.change-root-password.sh
@@ -0,0 +1,12 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+passwd -d root
+
+___END
