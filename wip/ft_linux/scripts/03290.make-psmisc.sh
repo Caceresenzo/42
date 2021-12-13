339bafafdcb3aff660d4da8880954df9210366eb
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-13 16:02:51 +0100

    WIP: ft_linux: Make `psmisc`

---
+++ wip/ft_linux/scripts/03290.make-psmisc.sh
@@ -0,0 +1,21 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf psmisc-*.tar*
+cd psmisc-*/
+
+./configure --prefix=/usr
+
+make
+
+make install
+
+___END
