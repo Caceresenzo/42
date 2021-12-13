9c3afcfb61de9c2c85e7b7bdf2dd81072ac02521
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-13 16:03:57 +0100

    WIP: ft_linux: Make `less`

---
+++ wip/ft_linux/scripts/03390.make-less.sh
@@ -0,0 +1,21 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf less-*.tar*
+cd less-*/
+
+./configure --prefix=/usr --sysconfdir=/etc
+
+make
+
+make install
+
+___END
