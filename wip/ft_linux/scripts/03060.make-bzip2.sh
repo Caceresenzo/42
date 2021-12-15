9ab27fb3e8c81bc55885f099c9eac82cd7f9a770
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-15 16:32:03 +0100

    WIP: ft_linux: Make `bzip2`: Fixing a bad `ln`

--- wip/ft_linux/scripts/03060.make-bzip2.sh
+++ wip/ft_linux/scripts/03060.make-bzip2.sh
@@ -25,7 +25,7 @@ make
 make PREFIX=/usr install
 
 cp -av libbz2.so.* /usr/lib
-ln -sv libbz2.so.* /usr/lib/libbz2.so
+ln -sv libbz2.so.1.0.8 /usr/lib/libbz2.so
 
 cp -v bzip2-shared /usr/bin/bzip2
 for i in /usr/bin/{bzcat,bunzip2}; do
