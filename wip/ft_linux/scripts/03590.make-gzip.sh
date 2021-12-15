278a9ef9338e93087daef717864b663fa8c08ba2
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-15 16:34:34 +0100

    WIP: ft_linux: Make `gzip`

---
+++ wip/ft_linux/scripts/03590.make-gzip.sh
@@ -0,0 +1,23 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf gzip-*.tar*
+cd gzip-*/
+
+./configure --prefix=/usr
+
+make
+
+make check
+
+make install
+
+___END
