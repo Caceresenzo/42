a869edefada068531dd394366edf9af46f1ebcd8
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-15 16:35:49 +0100

    WIP: ft_linux: Make `util linux`

---
+++ wip/ft_linux/scripts/03710.make-util-linux.sh
@@ -0,0 +1,41 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf util-linux-*.tar*
+cd util-linux-*/
+
+./configure ADJTIME_PATH=/var/lib/hwclock/adjtime   \
+            --libdir=/usr/lib    \
+            --docdir=/usr/share/doc/util-linux-2.37.2 \
+            --disable-chfn-chsh  \
+            --disable-login      \
+            --disable-nologin    \
+            --disable-su         \
+            --disable-setpriv    \
+            --disable-runuser    \
+            --disable-pylibmount \
+            --disable-static     \
+            --without-python     \
+            --without-systemd    \
+            --without-systemdsystemunitdir \
+            runstatedir=/run
+
+make
+
+# hangs forever...
+# chown -Rv tester .
+# su tester << EOF
+# make -k check
+# EOF
+
+make install
+
+___END
