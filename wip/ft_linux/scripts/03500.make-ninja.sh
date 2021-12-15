988ef2a6cf6a556157645706fe409d0e9b8a565d
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-15 16:33:23 +0100

    WIP: ft_linux: Make `ninja`

---
+++ wip/ft_linux/scripts/03500.make-ninja.sh
@@ -0,0 +1,33 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf ninja-*.tar*
+cd ninja-*/
+
+export NINJAJOBS=4
+
+sed -i '/int Guess/a \
+  int   j = 0;\
+  char* jobs = getenv( "NINJAJOBS" );\
+  if ( jobs != NULL ) j = atoi( jobs );\
+  if ( j > 0 ) return j;\
+' src/ninja.cc
+
+python3 configure.py --bootstrap
+
+./ninja ninja_test
+./ninja_test --gtest_filter=-SubprocessTest.SetWithLots
+
+install -vm755 ninja /usr/bin/
+install -vDm644 misc/bash-completion /usr/share/bash-completion/completions/ninja
+install -vDm644 misc/zsh-completion  /usr/share/zsh/site-functions/_ninja
+
+___END
