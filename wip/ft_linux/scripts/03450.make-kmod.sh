7c884feb25060315e8fc9178668507141e4dc790
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-15 16:32:45 +0100

    WIP: ft_linux: Make `kmod`

---
+++ wip/ft_linux/scripts/03450.make-kmod.sh
@@ -0,0 +1,31 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf kmod-*.tar*
+cd kmod-*/
+
+./configure --prefix=/usr          \
+            --sysconfdir=/etc      \
+            --with-xz              \
+            --with-zstd            \
+            --with-zlib
+
+make
+
+make install
+
+for target in depmod insmod modinfo modprobe rmmod; do
+  ln -sfv ../bin/kmod /usr/sbin/\$target
+done
+
+ln -sfv kmod /usr/bin/lsmod
+
+___END
