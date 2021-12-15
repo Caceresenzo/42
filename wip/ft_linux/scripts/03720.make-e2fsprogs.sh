dc79e4e60d0334730ea5790fb701dae28ed8477b
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-15 16:36:00 +0100

    WIP: ft_linux: Make `e2fsprogs`

---
+++ wip/ft_linux/scripts/03720.make-e2fsprogs.sh
@@ -0,0 +1,41 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf e2fsprogs-*.tar*
+cd e2fsprogs-*/
+
+mkdir -vp build
+cd        build
+
+../configure --prefix=/usr           \
+             --sysconfdir=/etc       \
+             --enable-elf-shlibs     \
+             --disable-libblkid      \
+             --disable-libuuid       \
+             --disable-uuidd         \
+             --disable-fsck
+
+make
+
+make check || true
+
+make install
+
+rm -fv /usr/lib/{libcom_err,libe2p,libext2fs,libss}.a
+
+gunzip -v /usr/share/info/libext2fs.info.gz
+install-info --dir-file=/usr/share/info/dir /usr/share/info/libext2fs.info
+
+makeinfo -o      doc/com_err.info ../lib/et/com_err.texinfo
+install -v -m644 doc/com_err.info /usr/share/info
+install-info --dir-file=/usr/share/info/dir /usr/share/info/com_err.info
+
+___END
