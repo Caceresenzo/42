9568010cc6b4f429ee66d276967be2bb37e4e8ea
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-15 16:34:13 +0100

    WIP: ft_linux: Make `groff`

---
+++ wip/ft_linux/scripts/03570.make-groff.sh
@@ -0,0 +1,21 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf groff-*.tar*
+cd groff-*/
+
+PAGE=A4 ./configure --prefix=/usr
+
+make -j1
+
+make install
+
+___END
