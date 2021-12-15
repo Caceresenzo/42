36b60bc8da1b0bf519c98619e620ff20da2347a9
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-15 16:33:11 +0100

    WIP: ft_linux: Make `openssl`

---
+++ wip/ft_linux/scripts/03480.make-openssl.sh
@@ -0,0 +1,30 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf openssl-*.tar*
+cd openssl-*/
+
+./config --prefix=/usr         \
+         --openssldir=/etc/ssl \
+         --libdir=lib          \
+         shared                \
+         zlib-dynamic
+
+make
+
+make test
+
+sed -i '/INSTALL_LIBS/s/libcrypto.a libssl.a//' Makefile
+make MANSUFFIX=ssl install
+
+cp -vfr doc/* /usr/share/doc/openssl
+
+___END
