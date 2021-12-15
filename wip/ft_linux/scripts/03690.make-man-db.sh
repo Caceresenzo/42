68831ac6a9561ea8c1394392bcfd1f8a2ce9f676
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-15 16:35:35 +0100

    WIP: ft_linux: Make `man db`

---
+++ wip/ft_linux/scripts/03690.make-man-db.sh
@@ -0,0 +1,32 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf man-db-*.tar*
+cd man-db-*/
+
+./configure --prefix=/usr                        \
+            --docdir=/usr/share/doc/man-db-2.9.4 \
+            --sysconfdir=/etc                    \
+            --disable-setuid                     \
+            --enable-cache-owner=bin             \
+            --with-browser=/usr/bin/lynx         \
+            --with-vgrind=/usr/bin/vgrind        \
+            --with-grap=/usr/bin/grap            \
+            --with-systemdtmpfilesdir=           \
+            --with-systemdsystemunitdir=
+
+make
+
+make check
+
+make install
+
+___END
