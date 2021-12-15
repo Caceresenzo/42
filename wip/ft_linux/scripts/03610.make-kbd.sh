13f8aa399ad817dd2e1ed32be10afd3b8a6e072f
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-15 16:34:48 +0100

    WIP: ft_linux: Make `kbd`

---
+++ wip/ft_linux/scripts/03610.make-kbd.sh
@@ -0,0 +1,31 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf kbd-*.tar*
+cd kbd-*/
+
+patch -Np1 -i ../kbd-*.patch
+
+sed -i '/RESIZECONS_PROGS=/s/yes/no/' configure
+sed -i 's/resizecons.8 //' docs/man/man8/Makefile.in
+
+./configure --prefix=/usr --disable-vlock
+
+make
+
+make check
+
+make install
+
+mkdir -vp            /usr/share/doc/kbd
+cp -R -v docs/doc/*  /usr/share/doc/kbd
+
+___END
