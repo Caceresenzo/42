0b9f6f4e4dbfbf1091b436feb562a4853617d1d3
Caceresenzo <caceresenzo1502@gmail.com>
2021-12-15 16:36:11 +0100

    WIP: ft_linux: Make `sysklogd`

---
+++ wip/ft_linux/scripts/03730.make-sysklogd.sh
@@ -0,0 +1,36 @@
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h        \
+    << ___END
+set -e
+
+cd /sources
+
+tar xvf sysklogd-*.tar*
+cd sysklogd-*/
+
+sed -i '/Error loading kernel symbols/{n;n;d}' ksym_mod.c
+sed -i 's/union wait/int/' syslogd.c
+
+make
+
+make BINDIR=/sbin install
+
+cat > /etc/syslog.conf << "EOF"
+# Begin /etc/syslog.conf
+
+auth,authpriv.* -/var/log/auth.log
+*.*;auth,authpriv.none -/var/log/sys.log
+daemon.* -/var/log/daemon.log
+kern.* -/var/log/kern.log
+mail.* -/var/log/mail.log
+user.* -/var/log/user.log
+*.emerg *
+
+# End /etc/syslog.conf
+EOF
+
+___END
