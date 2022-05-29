c1db56d7b002189128ebc1d558a5b3097fc0dcdc
Caceresenzo <caceresenzo1502@gmail.com>
2022-05-30 01:51:13 +0200

    WIP: computorv1: Added some tests

---
+++ wip/computorv1/test/run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+CASES_DIRECTORY=$1
+EXPECTED_EXIT_CODE=$2
+
+LOCATION_RIGHT=$(printf '\033[90G')
+COLOR_RED=$(printf '\033[31m')
+COLOR_GREEN=$(printf '\033[32m')
+COLOR_CYAN=$(printf '\033[36m')
+COLOR_RESET=$(printf '\033[0m')
+
+function print_outcome()
+{
+	color=$1
+	text=$2
+	
+	printf "%s%s%s$COLOR_RESET\n" "$LOCATION_RIGHT" "$color" "$text"
+}
+
+for file in $CASES_DIRECTORY/*
+do
+	name=$(basename "$file")
+	
+	./computor "$name" > /tmp/computor_output 2>&1
+	exit_code=$?
+	
+	printf "%s%s%s" "$COLOR_CYAN" "$name" "$COLOR_RESET"
+	
+	if [ $exit_code != $EXPECTED_EXIT_CODE ]
+	then
+		print_outcome "$COLOR_RED" "FAIL"
+	else
+		if diff --color=always --side-by-side --suppress-common-lines /tmp/computor_output "$file" > /tmp/computor_diff;
+		then
+			print_outcome "$COLOR_GREEN" "PASS ($exit_code)"
+		else
+			print_outcome "$COLOR_RED" "DIFF FAIL"
+			cat /tmp/computor_diff
+		fi
+	fi
+done
