1d63b2240b7ccd15f49707142ade5993f030ed91
Caceresenzo <caceresenzo1502@gmail.com>
2022-06-05 23:26:44 +0200

    WIP: computorv1: Added a lot of tests

---
+++ wip/computorv1/create-test.sh
@@ -0,0 +1,28 @@
+TEMPORARY_FILE=/tmp/computor-test
+
+COLOR_CYAN=$(printf '\033[36m')
+COLOR_RESET=$(printf '\033[0m')
+
+PREFIX="$COLOR_CYAN[creator]$COLOR_RESET"
+
+type=$1
+expression=$2
+
+echo "$PREFIX running: ./computor '$expression' 2>&1"
+
+./computor "$expression" 2>&1 | tee "$TEMPORARY_FILE"
+
+echo -n "$PREFIX create test? [yN] "
+read response
+
+if [ "$response" = "y" ]
+then
+	file="test/cases/$type/$expression"
+	
+	cp "$TEMPORARY_FILE" "$file"
+	echo "$PREFIX created: $file"
+else
+	echo "$PREFIX not created"
+fi
+
+rm $TEMPORARY_FILE
