8541e4192b3e1c62733e5f608fdc2ca31af108d9
Caceresenzo <caceresenzo1502@gmail.com>
2022-06-24 12:21:06 +0200

    WIP: nm: Splitted `symbol_helper.c` into multiple files

--- wip/nm/symbol_sort.c
+++ wip/nm/symbol_sort.c
@@ -17,66 +17,6 @@
 
 #include "nm.h"
 
-t_symbol*
-symbol_create(t_elf_address value, bool has_value, const char *name, char letter)
-{
-	t_symbol *symbol = malloc(sizeof(t_symbol));
-	if (symbol)
-	{
-		symbol->value = value;
-		symbol->has_value = has_value;
-		symbol->letter = letter;
-		symbol->name = name;
-	}
-
-	return (symbol);
-}
-
-static void
-symbol_print(int size, t_symbol *symbol)
-{
-	if (symbol->has_value)
-	{
-		char buffer[16 + 1];
-		ft_itoa((long)symbol->value, buffer, 16);
-
-		int len = ft_strlen(buffer);
-		if (len != size)
-		{
-			char zeros[16 + 1] = "0000000000000000";
-			zeros[size - len] = '\0';
-			ft_putstr_fd(zeros, STDOUT_FILENO);
-		}
-
-		ft_putstr_fd(buffer, STDOUT_FILENO);
-	}
-	else
-	{
-		char spaces[16 + 1] = "                ";
-		spaces[size] = '\0';
-
-		ft_putstr_fd(spaces, STDOUT_FILENO);
-	}
-
-	ft_putchar_fd(' ', STDOUT_FILENO);
-	ft_putchar_fd(symbol->letter, STDOUT_FILENO);
-	ft_putchar_fd(' ', STDOUT_FILENO);
-	ft_putstr_fd(symbol->name, STDOUT_FILENO);
-	ft_putchar_fd('\n', STDOUT_FILENO);
-}
-
-void
-symbol_print_x64(t_symbol *symbol)
-{
-	return (symbol_print(16, symbol));
-}
-
-void
-symbol_print_x32(t_symbol *symbol)
-{
-	return (symbol_print(8, symbol));
-}
-
 static const char*
 skip_underscores(const char *str)
 {
