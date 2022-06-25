c8dd80c1929824b9323438378eab5ee87d144b46
Caceresenzo <caceresenzo1502@gmail.com>
2022-06-25 22:23:26 +0200

    WIP: nm: Testing `-ag`

--- wip/nm/tests/run.sh
+++ wip/nm/tests/run.sh
@@ -6,8 +6,7 @@ make -s -C tests list-files | grep -v make | while read -r file
 do
 	sh tests/test.sh $file
 	
-	for flag in "-a" "-u" "-g" "-r" "-p" "-ap" "-ar" "-n" "-nr" "-up" "-ur" "-gp" "-gr"; do
-#	for flag in "-p" "-ap" "-up" "-gp"; do
+	for flag in "-a" "-u" "-g" "-r" "-p" "-ap" "-ar" "-n" "-nr" "-up" "-ur" "-gp" "-gr" "-ag"; do
 		sh tests/test.sh $flag $file
 	done
 done
