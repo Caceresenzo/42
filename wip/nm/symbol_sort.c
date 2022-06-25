29f7f8dfdf9d931f36295b8c975192d3ad7a56ec
Caceresenzo <caceresenzo1502@gmail.com>
2022-06-25 20:14:54 +0200

    WIP: nm: Better `-n` support

--- wip/nm/symbol_sort.c
+++ wip/nm/symbol_sort.c
@@ -36,17 +36,33 @@ symbol_list_compare_name_reversed(const void *a, const void *b)
 	return (-symbol_list_compare_name(a, b));
 }
 
+static bool
+symbol_is_undefined(const t_symbol *symbol)
+{
+	return (symbol->letter == 'U' || symbol->letter == 'w' || symbol->letter == 'v');
+}
+
 int
 symbol_list_compare_numeric(const void *a, const void *b)
 {
 	const t_symbol *left = *(const t_symbol**)a;
 	const t_symbol *right = *(const t_symbol**)b;
 
-	if (right->value == left->value)
-		return (0);
-	if (right->value > left->value)
-		return (-1);
-	return (1);
+	if (symbol_is_undefined(left))
+	{
+		if (!symbol_is_undefined(right))
+			return (-1);
+	}
+	else if (symbol_is_undefined(right))
+		return (1);
+	else if (left->value != right->value)
+	{
+		if (left->value < right->value)
+			return (-1);
+		return (1);
+	}
+
+	return (symbol_list_compare_name(a, b));
 }
 
 int
