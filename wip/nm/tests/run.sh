de8e3a43ba0cf55e90278a9865ef91cad6bcc18a
Caceresenzo <caceresenzo1502@gmail.com>
2022-06-25 20:15:50 +0200

    WIP: nm: Testing `-n`

--- wip/nm/tests/run.sh
+++ wip/nm/tests/run.sh
@@ -1,12 +1,12 @@
 set -e
 
-make -s -C tests
+make -C tests
 
 make -s -C tests list-files | grep -v make | while read -r file
 do
 	sh tests/test.sh $file
 	
-	for flag in "-a" "-u" "-g" "-r" "-p" "-ap" "-ar" "-up" "-ur" "-gp" "-gr"; do
+	for flag in "-a" "-u" "-g" "-r" "-p" "-ap" "-ar" "-n" "-nr" "-up" "-ur" "-gp" "-gr"; do
 #	for flag in "-p" "-ap" "-up" "-gp"; do
 		sh tests/test.sh $flag $file
 	done
