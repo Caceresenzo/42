d490f9fb501acb2d994d9bab3eff772d5dd22e44
Caceresenzo <caceresenzo1502@gmail.com>
2022-12-05 02:19:52 +0100

    WIP: matcha: Sorting paths for swagger

--- wip/matcha/backend/src/main/java/ft/framework/swagger/SwaggerBuilder.java
+++ wip/matcha/backend/src/main/java/ft/framework/swagger/SwaggerBuilder.java
@@ -1,7 +1,8 @@
 package ft.framework.swagger;
 
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.List;
+import java.util.TreeMap;
 
 import ft.framework.mvc.mapping.Route;
 import ft.framework.swagger.part.OperationBuilder;
@@ -13,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 public class SwaggerBuilder {
 	
 	public static Swagger build(Swagger swagger, List<Route> routes) {
-		final var paths = new HashMap<String, Path>();
+		final var paths = new TreeMap<String, Path>(Comparator.naturalOrder());
 		swagger.setPaths(paths);
 		
 		for (final var route : routes) {
