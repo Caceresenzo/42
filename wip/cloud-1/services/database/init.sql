8ef85559b0461d5aa729e5037bb133f42e81fa78
Caceresenzo <caceresenzo1502@gmail.com>
2023-03-10 22:19:41 +0100

    WIP: cloud-1: Added PhpMyAdmin

--- wip/cloud-1/services/database/init.sql
+++ wip/cloud-1/services/database/init.sql
@@ -4,4 +4,8 @@ CREATE DATABASE `wordpress`;
 CREATE USER 'wordpress'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON `wordpress`.* TO 'wordpress'@'%';
 
+CREATE DATABASE `phpmyadmin`;
+CREATE USER 'phpmyadmin'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'phpmyadmin'@'%';
+
 FLUSH PRIVILEGES;
