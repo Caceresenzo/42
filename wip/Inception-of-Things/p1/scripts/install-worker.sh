11f7bb5d16e11ac883550eac95be27b35d43d366
Caceresenzo <caceresenzo1502@gmail.com>
2023-05-28 22:23:57 +0200

    WIP: Inception-of-Things: Added project

---
+++ wip/Inception-of-Things/p1/scripts/install-worker.sh
@@ -0,0 +1,12 @@
+echo "Installing worker"
+
+export K3S_TOKEN_FILE="$PWD/token"
+export K3S_URL="https://$1:6443"
+
+export INSTALL_K3S_EXEC="agent --node-ip $2 --node-external-ip $2 -v 3"
+
+# k3s
+curl -sfL https://get.k3s.io | sh -
+
+# checks
+ifconfig eth1 | grep inet
