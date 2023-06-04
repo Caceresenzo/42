22933bf0079150241f408276f4de463d07e793d6
Caceresenzo <caceresenzo1502@gmail.com>
2023-06-04 21:32:23 +0200

    WIP: Inception-of-Things: p2: Removed docker provisioning

--- wip/Inception-of-Things/p2/scripts/deploy.sh
+++ wip/Inception-of-Things/p2/scripts/deploy.sh
@@ -1,1 +1,31 @@
-k apply -f /vagrant/confs
+function wait_all()
+{
+    namespace=$1
+
+    kubectl get -n $namespace deployment
+    
+    while true; do
+        services=$(kubectl get -n $namespace deployment --no-headers -o custom-columns=":metadata.name" | xargs -I{} echo deployment.apps/{})
+    
+        if [ -z "$services" ]; then
+            echo "no service in namespace '$namespace', retrying in 5 seconds"
+            sleep 5
+        else
+            break
+        fi
+    done
+
+    k wait --for=condition=available --timeout=300s -n $namespace $services
+}
+
+echo "[IoT] waiting for kubernetes' deployments to be available"
+wait_all kube-system
+
+echo "[IoT] adding deployment"
+k apply -f /vagrant/confs/app-1.yaml
+k apply -f /vagrant/confs/app-2.yaml
+k apply -f /vagrant/confs/app-3.yaml
+k apply -f /vagrant/confs/ingress.yaml
+
+echo "[IoT] waiting for deployments to be available"
+wait_all default
