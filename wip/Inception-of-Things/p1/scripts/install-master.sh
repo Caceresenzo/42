7663c5f3d2c1e44234f660ed96a96ee1e8d97067
Caceresenzo <caceresenzo1502@gmail.com>
2023-06-04 19:06:39 +0200

    WIP: Inception-of-Things: p1: Using k3s' kubectl

--- wip/Inception-of-Things/p1/scripts/install-master.sh
+++ wip/Inception-of-Things/p1/scripts/install-master.sh
@@ -1,4 +1,4 @@
-echo "Installing master"
+echo "[IoT] installing master"
 
 export K3S_TOKEN_FILE="/vagrant/confs/token"
 export K3S_KUBECONFIG_MODE="644"
@@ -6,24 +6,14 @@ export K3S_CLUSTER_INIT="true"
 
 export INSTALL_K3S_EXEC="server --tls-san $(hostname) --node-ip $1 --advertise-address=$1 --bind-address=0.0.0.0"
 
-# k3s
+echo "[IoT] installing k3s"
 curl -sfL https://get.k3s.io | sh -
 
-# kubectl
-curl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o /tmp/kubectl
-curl -L "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256" -o /tmp/kubectl.sha256
-echo "$(cat /tmp/kubectl.sha256)  /tmp/kubectl" | sha256sum --check
-sudo install -o root -g root -m 0755 /tmp/kubectl /usr/local/bin/kubectl
-rm /tmp/kubectl /tmp/kubectl.sha256
+echo "[IoT] aliasing k"
+path=/usr/local/bin/k
+echo '#!/bin/sh\nk3s kubectl $@' > $path
+chmod +x $path
 
-# "k" alias
-sudo ln -s $(which kubectl) $(dirname $(which kubectl))/k
-
-# setting environment
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-sudo chmod +r $KUBECONFIG
-echo "KUBECONFIG=$KUBECONFIG" >> /etc/environment
-
-# checks
+echo "[IoT] doing additional checks"
 k get nodes -o wide
 ifconfig eth1 | grep inet
--- wip/Inception-of-Things/p1/scripts/install-worker.sh
+++ wip/Inception-of-Things/p1/scripts/install-worker.sh
@@ -1,12 +1,12 @@
-echo "Installing worker"
+echo "[IoT] installing worker"
 
 export K3S_TOKEN_FILE="/vagrant/confs/token"
 export K3S_URL="https://$1:6443"
 
 export INSTALL_K3S_EXEC="agent --node-ip $2 --node-external-ip $2 -v 3"
 
-# k3s
+echo "[IoT] installing k3s"
 curl -sfL https://get.k3s.io | sh -
 
-# checks
+echo "[IoT] doing additional checks"
 ifconfig eth1 | grep inet
