489b5c578ddd67320f5ab31da4b73dcca9632d27
Caceresenzo <caceresenzo1502@gmail.com>
2023-09-16 16:53:45 +0200

    WIP: Inception-of-Things: p1: Using latest debian version

--- wip/Inception-of-Things/p1/scripts/install-worker.sh
+++ wip/Inception-of-Things/p1/scripts/install-worker.sh
@@ -3,7 +3,7 @@ echo "[IoT] installing worker"
 export K3S_TOKEN_FILE="/vagrant/confs/token"
 export K3S_URL="https://$1:6443"
 
-export INSTALL_K3S_EXEC="agent --node-ip $2 --node-external-ip $2 -v 3"
+export INSTALL_K3S_EXEC="agent --node-ip $2 -v 3"
 
 echo "[IoT] installing k3s"
 curl -sfL https://get.k3s.io | sh -
