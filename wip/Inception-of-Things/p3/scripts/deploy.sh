821c4a027d4d3e29dcc5888b264983a10d6bdb10
Caceresenzo <caceresenzo1502@gmail.com>
2023-09-23 12:33:36 +0200

    WIP: Inception-of-Things: p3: Using Vagrant only for starting the VM

--- wip/Inception-of-Things/p3/scripts/deploy.sh
+++ wip/Inception-of-Things/p3/scripts/deploy.sh
@@ -1,3 +1,10 @@
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run as root." >&2;
+    exit 1;
+fi
+
 function wait_all()
 {
     namespace=$1
--- wip/Inception-of-Things/p3/scripts/install-tools.sh
+++
@@ -1,5 +0,0 @@
-echo "[IoT] installing tools"
-apt update
-apt install net-tools curl -y
-
-echo -e 'export PATH="/usr/sbin:$PATH"' >> /etc/profile
