b68f98bf66fb554ad34ea7bebe2da4c8fd95be92
Caceresenzo <caceresenzo1502@gmail.com>
2023-10-07 20:21:26 +0200

    WIP: BADASS: P3: Removed superfluous parameters

--- wip/Bgp At Doors of Autonomous Systems is Simple/P3/_ecaceres-4.sh
+++ wip/Bgp At Doors of Autonomous Systems is Simple/P3/_ecaceres-4.sh
@@ -1,6 +1,6 @@
 ip link add br0 type bridge
 ip link set dev br0 up
-ip link add vxlan10 type vxlan id 10 dev eth2 dstport 4789
+ip link add vxlan10 type vxlan id 10 dstport 4789
 ip link set dev vxlan10 up
 brctl addif br0 vxlan10
 brctl addif br0 eth0
